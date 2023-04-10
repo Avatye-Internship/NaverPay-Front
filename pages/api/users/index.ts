8e63ef08230afd176166ab53e96e22a291ef2521
Ryuhyis <skyblue22756@gmail.com>
2023-04-10 14:10:37 +0900

    test추가

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,18 +1,23 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
 
+const basicUrl = process.env.SERVER_API_URL
 export default async function userHandler(req: NextApiRequest, res: NextApiResponse) {
+    let result;
     switch (req.method) {
+        
         // 내 정보 조회
         case 'GET':
-            const result = await fetch('/api')
+            result = await fetch(basicUrl+'/api/users')
             res.send(result)
             break;
         // 회원가입
         case 'POST':
+            result = await fetch(basicUrl+'/api/users')
             break;
         // 내 정보 수정
         case 'PATCH':
+            result = await fetch(basicUrl+'/api/users')
             break;
     }
 }
